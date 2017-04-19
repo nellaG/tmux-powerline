ee875a6eb7279198ab6a46688e3009b08592bc29
nellaG <seirios0107@gmail.com>
2017-04-20 01:14:40 +0900

    change color pallete similar to nord theme

--- themes/default.sh
+++ themes/default.sh
@@ -12,6 +12,7 @@ else
 	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
+# LEFT BOLD background color
 TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
 TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
 
@@ -22,13 +23,18 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 # Format: segment_name background_color foreground_color [non_default_separator]
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
+  # "in segment_name {1} {2} , {1}, {2} is color palette number.
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		#"tmux_session_info 148 234" \
+		"tmux_session_info 4 255" \
+		#"hostname 33 0" \
+		"hostname 12 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		"lan_ip 10 0 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		#"wan_ip 24 255" \
+		"wan_ip 10 0" \
 		"vcs_branch 29 88" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
@@ -51,9 +57,12 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		"weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"date_day 235 136" \
+		"date_day 235 6" \
+		#"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date 235 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time 235 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
